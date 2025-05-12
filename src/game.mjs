0d9db257b050b74ca1b917765bd3faafa28a541a
anastat <anastasiia.tikhonova@mail.com>
2025-05-12 20:13:23 +0300

    Refactoring: moved logic of game simulation to game.mjs

--- src/game.mjs
+++ src/game.mjs
@@ -1,18 +1,3 @@
-import fs from "fs/promises";
-import { parseContent } from "./parseContent.mjs";
-
-export async function main(path, iterations) {
-  const fileContent = await fs.readFile(path, { encoding: "utf8" });
-
-  const { hashLines, headerLine, patternLinesStr } = parseContent(fileContent);
-
-  return writeToFile(hashLines, headerLine, patternLinesStr);
-}
-
-export function writeToFile(hashLines, headerLine, patternLinesStr) {
-  return hashLines.join("\n") + "\n" + headerLine + "\n" + patternLinesStr;
-}
-
 export function getNeighbours({ x, y }) {
   let neighbours = [];
   let xCell, yCell;
